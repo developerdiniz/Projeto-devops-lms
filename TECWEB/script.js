23fcf7b164221830847ab16daaeaae7b0a46d56b
teamdiniz <42000525+teamdiniz@users.noreply.github.com>
2018-10-23 10:19:37 -0300

    Cadastro Disciplina
    upload cadastro disciplina, script e css

---
+++ TECWEB/script.js
@@ -0,0 +1,14 @@
+function enviar(){
+    var nome = document.getElementById("nome");
+    var data = document.getElementById("data");
+    var plano = document.getElementById("plano");
+    var carga = document.getElementById("carga");
+    var competencias = document.getElementById("competencias");
+    var habilidades = document.getElementById("habilidades");
+    var emenda = document.getElementById("emenda");
+    var programatico = document.getElementById("programatico");
+    var bibliografia = document.getElementById("bibliografia");
+    var biblioteca = document.getElementById("biblioteca"); 
+
+
+    
